a076453125fd02726976cb9ff426d50d1491fe50
Beviryon ISSANGA-NGOULOU <beviryon.issanga-ngoulou@eurexo-ced.fr>
2024-06-13 09:51:06 +0200

    mise en place des produits

--- data.js
+++ data.js
@@ -54,22 +54,21 @@ const products = [
       description: 'Jupe en denim pour femmes, longue, taille haute, mode de bureau, style décontracté, nouvelle collection été',
       price: 24.99,
       images: [
-        'https://images.pexels.com/photos/7974/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600',
-        'https://images.pexels.com/photos/7974/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600',
-        'https://images.pexels.com/photos/7974/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600'
-      ],
+        'https://s.alicdn.com/@sc04/kf/H85526e7e55e443c593b9302487ad2ba2R.jpg',
+        'https://s.alicdn.com/@sc04/kf/H1f7aaa81232649b4b352c4e228268009O.jpg',
+        'https://s.alicdn.com/@sc04/kf/Hfde0ff53a70a41ad82d118ee575586ffY.jpg'
+        ],
       vendorPhone: '+33758146261' 
     },
     {
       id: 6,
-      name: 'Produit 2',
-      description: 'Description du produit 2',
-      price: 24.99,
-
+      name: 'Robe',
+      description: 'Tailles: XL | M | L',
+      price: 8000,
       images: [
-        'https://images.pexels.com/photos/7974/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600',
-        'https://images.pexels.com/photos/7974/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600',
-        'https://images.pexels.com/photos/7974/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600'
+        'https://db3pap006files.storage.live.com/y4mc4hcMYmNr5YU88xwxNSDuV7FGAc2ej1B9KZWIoH_GyulchS8x60OVcTKvw772UbpWsrNP_tDeHdMsgUb-rRDReIECOkCJGXERoCS4I1lUFdS94tKbXI7dIxKvBvukqJ5LkoiDUnDsjZDrdXt7hSxUYL-JQ6rO1trsdRWmmy5ZTRnLrbIbsdhhmE2lYjaKFyQ-E1-TEZkcVYFBTFEYYRXws7kcFvItC2teq3VNy2iD3c?encodeFailures=1&width=756&height=756',
+        'https://db3pap006files.storage.live.com/y4m6QsZYenD0EYz8xg9YKmdpB-Mdg4fZy4flV44gaUGjkKd-YI6liCeDYeRPLKeXQffal47ZLCKMDkD1oNvWB3bi76uo_tjNO1oYLwuYG9aJ75UoC_tIdqmUespmUCABmQuiOPr9-t8S6HkJyJkor1BeIFMglBkWKPplONYqJwfv8NXUuFDXEX644wfZ6jj18m6kDCkqEPJQag0vGrc0STMniu-Fk_Qrfq8QDmdRLe4Anw?encodeFailures=1&width=604&height=756',
+        'https://db3pap006files.storage.live.com/y4mGU8rgGiHvZrOmDKkz2S2t3Xu07Q7sYrW8m5f1t2_H83wZVKf_Ih4vZTvN0eIwF-SeqqtnewsPj003HF4l6qbEpJ9HESu6sPxxHl7Gvf2q3kwK7vMZQE_muCtC1fGFDtQcjrKpskc20BqsUAdVSSpqLLZh-MUbii0ykg9U23M7Yozr9rGHarKHClo8E_YoOUu387_8Lt5AOFIY7Vx66QvY38x_kzOVykJ3nH-7JkdjfU?encodeFailures=1&width=605&height=756'
       ],
       vendorPhone: '+33758146261' 
     },
@@ -77,15 +76,14 @@ const products = [
     // Vêtements
     {
         id: 7,
-        name: 'Produit 2',
-        description: 'Description du produit 2',
-        price: 24.99,
-
+        name: 'Robe en gros et en détails',
+        description: 'Tissu en coton,\n Disponible taille L | XL | XXL | 3XL',
+        price: 8000,
         images: [
-          'https://images.pexels.com/photos/3755706/pexels-photo-3755706.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-          'https://images.pexels.com/photos/6069553/pexels-photo-6069553.jpeg?auto=compress&cs=tinysrgb&w=600',
-          'https://images.pexels.com/photos/1389449/pexels-photo-1389449.jpeg?auto=compress&cs=tinysrgb&w=600'
-        ],
+          'https://db3pap006files.storage.live.com/y4mNmL6ftkc8Fw9dAxv7W0lBHmd3iwyVD3nfs7Z5pR_ef0rb_-Z4NuloqmB9AfqdbNOc8SgQ12gMRRYJkEqODAopmr8Ew6CxwyX_E9_YVQYGrS1w8FrhSv2puxH8Nz6LqhGn1Mnzn1iiAyEtdYJdU1sE3hO4RB7d9MtIzA_5wbBtlkxNTbTAa9Vs6rPDNiIDukmrJv7MQTQWW7siXoN-LChIphEqlqoNNcZMyZXXm53W2I?encodeFailures=1&width=503&height=755',
+          'https://db3pap006files.storage.live.com/y4mTSBEzwFzRpE5m3-yZSTF1z4ziIJf1lZmsIYnjWgzzWQLTh666HAtRL_Jd-ahi_HuKHNvqsy4twNPOOfeWtMaDB0pVcADZtdJwOAzKsaUwwJd5fCKfvbJEMzhthk_ZvPmKnqd6dUzN29JPs-7WXFJfeo0YsstcxUxny641JUktL5Ems0zDUxDaRGZ36DEUZ7kVgfeWATct5Zeq1GRIiaPnVbuHLhCLmQO-5XOgr6ZWS4?encodeFailures=1&width=503&height=756',
+          'https://db3pap006files.storage.live.com/y4m7hIf1N0BiNgEo5IefCxU-c71MbB7Bh_VOnFXNfNlGiRNz4MoWX8YrSqtVy8WCbveD3896VXhtPFgezq0mv6SsbcEA8xCe15YgTF_JhJi8mjq-acwHsYvS8VTCxIVNLM76MM-X3kIJt3NI3tnDhUIZIJ7j0FbazygySfjG15LxHQeCzfyjQ5yJOxavxsd6OfiERCQORA8dYRV4lz3oBO4v64vm6dhMeKS34Qy39Jzo5M?encodeFailures=1&width=503&height=756'
+          ],
         vendorPhone: '+33758146261' 
       },
     {
