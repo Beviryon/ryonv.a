4bf55c462c3c0d80759f586e116ec3cf2bcacf69
Beviryon ISSANGA-NGOULOU <beviryon.issanga-ngoulou@eurexo-ced.fr>
2024-07-15 14:51:31 +0200

    PWA

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,26 @@
 self.addEventListener('install', event => {
-    event.waitUntil(
-      caches.open('v1').then(cache => {
-        return cache.addAll([
-          '/',
-          './home.html',
-          './produits.html',
-          './services.html',
-          './about.html',
-          './contact.html',
-          './formations.html',
-          './produits.js',
-          './products.js',
-          './manifest.json',
-          './about.css',
-          './contact.css',
-          './services.css',
-          './produits.css',
-          './formations.css',
-          './home.css'
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', event => {
-    event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request, {mode: 'no-cors' });
-      })
-    );
-  });
-  
+  event.waitUntil(
+    caches.open('v1').then(cache => {
+      return cache.addAll([
+        '/',
+        './home.html',
+        './produits.html',
+        './services.html',
+        './about.html',
+        './contact.html',
+        './formations.html',
+        './produits.js',
+        './products.js',
+        './manifest.json',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
