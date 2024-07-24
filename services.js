876cfa71e3e0ac44b9f6943e5079f68d15894023
Beviryon ISSANGA-NGOULOU <beviryon.issanga-ngoulou@eurexo-ced.fr>
2024-07-24 15:39:29 +0200

    modification pwa et chargement page automatique

--- services.js
+++ services.js
@@ -1,12 +1,16 @@
-const navSlide = () => {
+document.addEventListener('DOMContentLoaded', () => {
+  // Fonction pour le menu burger
+  const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
   
-    burger.addEventListener('click', () => {
-      nav.classList.toggle('nav-active');
-      burger.classList.toggle('toggle');
-    });
+    if (burger && nav) {
+      burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        burger.classList.toggle('toggle');
+      });
+    }
   
     navLinks.forEach((link) => {
       link.addEventListener('click', () => {
@@ -17,34 +21,32 @@ const navSlide = () => {
   };
   
   navSlide();
-  
-{/* <script nonce="xyz"> */}
-document.addEventListener('DOMContentLoaded', () => {
+
+  // Gestion du pop-up
   const popup = document.getElementById('popup');
   const closePopup = document.querySelector('.close-popup');
 
-  // Afficher le pop-up au chargement de la page
-  function showPopup() {
-    popup.style.display = 'block';
+  if (popup && closePopup) {
+    // Afficher le pop-up au chargement de la page
+    function showPopup() {
+      popup.style.display = 'block';
+    }
+
+    // Fermer le pop-up
+    closePopup.addEventListener('click', () => {
+      popup.style.display = 'none';
+    });
+
+    // Afficher le pop-up après un délai ou immédiatement
+    setTimeout(showPopup, 1000); // Délai de 1 seconde
   }
 
-  // Fermer le pop-up
-  closePopup.addEventListener('click', () => {
-    popup.style.display = 'none';
-  });
-
-  // Afficher le pop-up après un délai ou immédiatement
-  setTimeout(showPopup, 1000); // Délai de 1 seconde
-});
-
+  // Service Worker
   if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('./service-worker.js').then(registration => {
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }, error => {
-        console.log('ServiceWorker registration failed: ', error);
-      });
+    navigator.serviceWorker.register('./service-worker.js').then(registration => {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }).catch(error => {
+      console.log('ServiceWorker registration failed: ', error);
     });
   }
-
-{/* </script> */}
+});
