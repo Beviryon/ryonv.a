8e1368badf56c0baf3c13ff1a0fb046b1bf56f5b
Beviryon ISSANGA-NGOULOU <beviryon.issanga-ngoulou@eurexo-ced.fr>
2024-07-24 16:39:18 +0200

    modification pwa et chargement page automatique

--- scrpt_pwa.js
+++ scrpt_pwa.js
@@ -18,19 +18,19 @@ function showInstallPromotion() {
   bottom: 20px;
   left: 50%;
   transform: translateX(-50%);
-  background: rgba(0, 0, 0, 0.8);
+  background: rgba(0, 0, 0, 0.9);
   color: #fff;
   padding: 1em 2em;
   text-align: center;
   border-radius: 10px;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.3);
   animation: pulse 2s infinite;
   z-index: 1000;
 ">
   <p style="margin: 0 0 10px 0; font-size: 16px;">Installez notre application pour une meilleure expérience !</p>
   <button id="installButton" style="
     padding: 0.7em 1.5em;
-    background: linear-gradient(45deg, #4CAF50, #45a049);
+    background: linear-gradient(45deg, #f2ae01, #f2ae01);
     color: #fff;
     border: none;
     border-radius: 5px;
@@ -44,10 +44,13 @@ function showInstallPromotion() {
 <style>
 @keyframes pulse {
   0% {
-    opacity: 1;
+    opacity: 2;
   }
   50% {
-    opacity: 0.7;
+    opacity: 0.1;
+  }
+  70% {
+    opacity: 1;
   }
   100% {
     opacity: 1;
